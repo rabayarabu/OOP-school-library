5cef35f2129a1350388c031b5ad060f4adfffd41
rabayarabu <rabu9149@gmail.com>
2023-08-29 16:24:19 +0600

    Fixed linters errors

--- student.rb
+++ student.rb
@@ -2,6 +2,7 @@ require_relative 'Person'
 
 class Student < Person
   attr_reader :classroom
+
   def initialize(age, classroom, parent_permission: false, name: '')
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
