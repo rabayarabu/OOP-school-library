6d642d3d7329524f59856c7891a6c79ce6b1e882
clarence <tegumclarence@gmail.com>
2023-09-15 06:49:43 +0100

    Fix linters

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Person do
       expect(person_without_name.name).to eq('Unknown')
     end
 
-    it "generates a random id between 1 and 10,000" do
+    it 'generates a random id between 1 and 10,000' do
       ids = []
       1000.times do
         person = Person.new(18, 'John Doe')
@@ -24,32 +24,31 @@ RSpec.describe Person do
 
       expect(ids).to all(be_between(1, 10_000).inclusive)
     end
-  
+
     it 'initializes rentals as an empty array' do
       expect(person.rentals).to be_empty
     end
   end
-  describe "#correct_name" do
-  it "returns the name of the person" do
-    expect(person.send(:correct_name)).to eq('John Doe')
+  describe '#correct_name' do
+    it 'returns the name of the person' do
+      expect(person.send(:correct_name)).to eq('John Doe')
+    end
   end
-end
 
-describe "#can_use_services?" do
-    it "returns true for a person of age 18" do
+  describe '#can_use_services?' do
+    it 'returns true for a person of age 18' do
       person = Person.new(18, 'John Doe')
       expect(person.send(:can_use_services?)).to be true
     end
 
-    it "returns true for a person with parent permission" do
+    it 'returns true for a person with parent permission' do
       person = Person.new(16, 'Alice', parent_permission: true)
       expect(person.send(:can_use_services?)).to be true
     end
 
-    it "returns false for a person under 18 without parent permission" do
+    it 'returns false for a person under 18 without parent permission' do
       person = Person.new(16, 'Bob', parent_permission: false)
       expect(person.send(:can_use_services?)).to be false
     end
   end
-  
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,21 +1,19 @@
 require_relative '../student'
 
 describe Student do
- 
-  describe "#initialize" do
-    it "creates a Student object with the correct attributes" do
-      student = Student.new(16, "Alice", parent_permission: true)
+  describe '#initialize' do
+    it 'creates a Student object with the correct attributes' do
+      student = Student.new(16, 'Alice', parent_permission: true)
 
       expect(student.age).to eq(16)
-      expect(student.name).to eq("Alice")
+      expect(student.name).to eq('Alice')
       expect(student.parent_permission).to be(true)
     end
   end
 
-
-  describe "#play_hooky" do
-    it "returns the shrug emoticon" do
-      student = Student.new(17, "Bob")
+  describe '#play_hooky' do
+    it 'returns the shrug emoticon' do
+      student = Student.new(17, 'Bob')
 
       result = student.play_hooky
 
@@ -23,34 +21,33 @@ describe Student do
     end
   end
 
-  describe "#classroom=" do
-  it "sets the classroom and adds the student to it" do
-    student = Student.new(15, "Charlie")
-    classroom = double("Classroom")
-    
-    # Stub the students method to return an empty array initially.
-    allow(classroom).to receive(:students).and_return([])
+  describe '#classroom=' do
+    it 'sets the classroom and adds the student to it' do
+      student = Student.new(15, 'Charlie')
+      classroom = double('Classroom')
 
-    # Expect the push method to be called with the student.
-    expect(classroom.students).to receive(:push).with(student)
-    
-    student.classroom = classroom
-    
-    expect(student.classroom).to eq(classroom)
-  end
+      # Stub the students method to return an empty array initially.
+      allow(classroom).to receive(:students).and_return([])
 
-  it "does not add the student to the classroom if already present" do
-    student = Student.new(14, "David")
-    classroom = double("Classroom")
-    
-    allow(classroom).to receive(:students).and_return([student])
-    
-    expect(classroom.students).not_to receive(:push)
-    
-    student.classroom = classroom
-    
-    expect(student.classroom).to eq(classroom)
+      # Expect the push method to be called with the student.
+      expect(classroom.students).to receive(:push).with(student)
+
+      student.classroom = classroom
+
+      expect(student.classroom).to eq(classroom)
+    end
+
+    it 'does not add the student to the classroom if already present' do
+      student = Student.new(14, 'David')
+      classroom = double('Classroom')
+
+      allow(classroom).to receive(:students).and_return([student])
+
+      expect(classroom.students).not_to receive(:push)
+
+      student.classroom = classroom
+
+      expect(student.classroom).to eq(classroom)
+    end
   end
-end
- 
 end
